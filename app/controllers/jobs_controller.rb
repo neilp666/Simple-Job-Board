4e52b90cf8083875cc9fe63f0b60e640b36c46d0
neil patel <neil@myindianweddingapp.com>
2014-10-22 10:55:06 +0100

    Updated edit and destory routes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,12 +4,28 @@ class JobsController < ApplicationController
 		@jobs = Job.all
 	end
 
+	def edit
+		@job = Job.find(params[:id])
+	end
+
 	def new
 		@job = Job.new
 	end
 
 	def create
 		Job.create(job_params)
+		redirect_to jobs_path
+	end
+
+	def update
+		@job = Job.find(params[:id])
+		@job.update(job_params)
+		redirect_to jobs_path
+	end
+
+	def destroy
+		@job = Job.find(params[:id])
+		@job.destroy
 		redirect_to jobs_path
 	end
 
